b70b50104c96dd2a0debcaa0a3c2025798e72775
Amna <ad9795@student.jamk.fi>
2023-09-14 11:06:04 +0300

    initial

---
+++ powershell/powershell-1/task02.ps1
@@ -0,0 +1,11 @@
+# Task 02
+# Ask from a user his/her firstname, lastname and groupcode. Save them to variables. After that show the following info in console.
+
+$firstname = Read-Host 'Please, give your firstname'
+$lastname = Read-Host 'Please, give your lastname'
+$groupcode = Read-Host 'Please, give your groupcode'
+
+Write-Host 'Welcome to couurse:' $firstname $lastname $groupcode
+
+# NOTES
+# Read-Host automatically adds a `:` and space at the end 
