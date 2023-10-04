406e846269b729e50432d1ccc22d73efc0d222f2
Amna <ad9795@student.jamk.fi>
2023-10-04 12:44:19 +0300

    initial

--- powershell/powershell-2/task01.ps1
+++ powershell/powershell-2/task01.ps1
@@ -1,3 +1,7 @@
+<# 
 # TASK 1
-# Create a script that writes current logged username and current date to console.
-Get-ChildItem
+Create a script that counts how many files are in user's home folder. The script shows the home folder name and the number of the files.
+#>
+$fileCount = (Get-ChildItem -Path $HOME -File).Count
+
+Write-Host ("$fileCount files found at $HOME")
