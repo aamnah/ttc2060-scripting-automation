4710f5c833f2575e807f7b7c7adb4d49f57b9474
Amna <ad9795@student.jamk.fi>
2023-10-04 12:50:16 +0300

    initial

--- powershell/powershell-2/task02.ps1
+++ powershell/powershell-2/task02.ps1
@@ -1,3 +1,7 @@
 # TASK 2
-# Create a script that counts how many files and folders are in given folder. The script shows the name of the folder and the number of the files.
-Get-ChildItem
+# Create a script that counts how many files and folders are in given folder. 
+# The script shows the name of the folder and the number of the files.
+$folderLocation = Read-Host 'Please, give the folder to search'
+$fileCount = (Get-ChildItem -Path $folderLocation).Count
+
+Write-Host "$fileCount files found at $folderLocation"
