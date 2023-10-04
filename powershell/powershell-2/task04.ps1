0cc8a6839e8869dcaf46bc78a141ac715b07ad46
Amna <ad9795@student.jamk.fi>
2023-10-04 13:17:34 +0300

    initial

--- powershell/powershell-2/task04.ps1
+++ powershell/powershell-2/task04.ps1
@@ -2,3 +2,13 @@
 # Create a script that takes two named parameters. 
 # The first parameter is the name of a user, second how many times the script greets the user. 
 # Give default values for the parameters, so if the script is run without arguments, it will say Hello unknown only once.
+
+param(
+  [string]$name = 'Unknown',
+  [int]$hellos = 1
+)
+
+for ($i = 0; $i -lt $hellos; $i++ ) {
+  Write-Host "Hello $name"
+}
+
