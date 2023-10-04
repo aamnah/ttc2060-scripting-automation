0ef1959664d4225bbfd32dea77dc8c776971f864
Amna <ad9795@student.jamk.fi>
2023-10-04 12:13:39 +0300

    +

--- powershell/powershell-1/task01.ps1
+++ powershell/powershell-1/task01.ps1
@@ -1,6 +1,6 @@
 # TASK 1
 # Create a script that writes current logged username and current date to console.
-$loggedInUser = '' 
+$loggedInUser = whoami
 $dateToday = Get-Date -Format d
 
 Write-Host 'Logged username is' $loggedInUser 'and today is' $dateToday
