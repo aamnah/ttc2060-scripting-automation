7e83ae827791f7b81462e07d0cb6c60f36e80d3b
Amna <ad9795@student.jamk.fi>
2023-10-04 12:13:55 +0300

    fix: typo

--- powershell/powershell-1/task02.ps1
+++ powershell/powershell-1/task02.ps1
@@ -5,7 +5,7 @@ $firstname = Read-Host 'Please, give your firstname'
 $lastname = Read-Host 'Please, give your lastname'
 $groupcode = Read-Host 'Please, give your groupcode'
 
-Write-Host 'Welcome to couurse:' $firstname $lastname $groupcode
+Write-Host 'Welcome to course:' $firstname $lastname $groupcode
 
 # NOTES
 # Read-Host automatically adds a `:` and space at the end 
