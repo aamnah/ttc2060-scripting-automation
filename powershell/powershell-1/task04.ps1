23ceb7f759e1eb3e518241fe1ac94145b1194a90
Amna <ad9795@student.jamk.fi>
2023-10-04 12:24:10 +0300

    +

--- powershell/powershell-1/task04.ps1
+++ powershell/powershell-1/task04.ps1
@@ -3,4 +3,6 @@
 # Create a script that show how many items is in the array, show the items and also the first and last name of the array.
 $names = @('Adeel', 'Raheel', 'Waqas', 'Soban', 'Safwan', 'Umar', 'Sannan')
 
-Write-Host $names
+Write-Host "There are $($names.Count) friends:"
+Write-Host $names.ForEach({Write-Host $_})
+Write-Host "The oldest is $($names[0]) and the youngest is $($names[6])"
