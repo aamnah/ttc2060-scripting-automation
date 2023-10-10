97b617148fcb4684819f058041012ae1b38a97e3
Aamnah <hello@aamnah.com>
2023-10-10 21:59:43 +0300

    +

--- powershell/powershell-3/task02.ps1
+++ powershell/powershell-3/task02.ps1
@@ -6,3 +6,20 @@ If true, show a proper message.
 If it does not exist, show a message: 
 "Sorry, {filename} in the {foldername} does not exist.". 
 #>
+param (
+  [string]$folder = '',
+  [string]$file = ''
+)
+
+$filepath = Join-Path -Path $folder -ChildPath $file
+
+if (Test-Path $folder)
+{
+  if (Test-Path $filepath) {
+    Write-Host "The file you are looking for exists in ${folder}"
+  } else {
+    Write-Host "Sorry, ${file} in ${folder} does not exist."
+  }
+} else {
+  Write-Host "${folder} does not exist"
+}
