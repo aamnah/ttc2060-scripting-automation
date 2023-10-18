19d0c9943308dcd1935816f2dfa06a79a62c4265
Aamnah <hello@aamnah.com>
2023-10-18 20:36:34 +0300

    initial

--- powershell/powershell-5/task05.ps1
+++ powershell/powershell-5/task05.ps1
@@ -1,10 +1,21 @@
 <# 5.5
-[ ] In this task you have to create a new function called Add-MyEvent. 
-[ ] The function takes two parameter: Type and Message. 
-[ ] The type can be: Error, Warning, or Information. 
+[x] In this task you have to create a new function called Add-MyEvent. 
+[x] The function takes two parameter: Type and Message. 
+[x] The type can be: Error, Warning, or Information. 
 [ ] Message is a clear message. 
 [ ] Add the following functionality to the task 04 using your new function. 
   [ ] - if the script can create a new CSV-file succesfully, then it will add a Information message to the eventlog. 
   [ ] - if the script cannot create a file succesfully, then it adds a Error message to the eventlog.
 [ ] Before using your new function create a new source named "MyPowerShell" to the Evenlog for writing your events. 
 #>
+
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-7.3#validateset-attribute
+
+function Add-MyEvent() {
+  param (
+    [ValidateSet("Error", "Warning", "Information")]
+    [string]$Type = '',
+    [string]$Message = ''
+  )
+
+}
