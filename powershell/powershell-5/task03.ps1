9d0d8f403f536c169ca3d34036de35554533876b
Aamnah <hello@aamnah.com>
2023-10-18 20:04:58 +0300

    needs testing

--- powershell/powershell-5/task03.ps1
+++ powershell/powershell-5/task03.ps1
@@ -1,27 +1,59 @@
 <# 5.3
-[ ] Create a script that creates new localusers by a given csv-file. 
-[ ] The script has one parameter filename. 
-[ ] Check first that the given file exist, 
+[x] Create a script that creates new localusers by a given csv-file. 
+[x] The script has one parameter filename. 
+[x] Check first that the given file exist, 
 [ ] then read all lines and create a new local user account for every user in file. 
-[ ] The account name is created same way as in the task 01. 
-[ ] You can use in this script the parameter NoPassword 
+[x] The account name is created same way as in the task 01. 
+[x] You can use in this script the parameter NoPassword 
 
-Please remember: you have to run this script with Administrator privileges that you can use the New-LocalUser cmdlet. 
+Please remember: you have to run this script with Administrator privileges that you can use the `New-LocalUser` cmdlet. 
 
 If the given file does not exist, the script gives a proper error message.
+
+# CSV
+# Firstname,Lastname
+# Jack,Russell
+# Anna,Bella
+# Nicke,Nackerstrom
 #>
 
 param(
   [string]$Filename = '' # path to CSV file containing names
 )
 
-if ($PSBoundParameters -eq 0) {
+$accounts = @()
+
+if ($PSBoundParameters.Count -eq 0) {
   Write-host "You need to provide a filename."
   Write-Host "Example usage: ./task03.ps1 -Filename 'users.csv'"
 } else {
   if (Test-Path $Filename) {
-    # Do what needs doing
+    # Import names from CSV and loop over them
+    Import-Csv $Filename | ForEach-Object {
+      $firstname = $_.Firstname
+      $lastname = $_.Lastname
+      $username = "$($lastname.Substring(0,4))$($firstname.Substring(0,2))".ToLower()
+
+      # Figure out which accounts should be cerated by confirming with the script runner
+      $consent = Read-Host "A new local account ${username} will be created for $firstname $lastname, [Yes/No]"
+
+      if (($consent.ToLower() -eq 'Yes') -Or ($consent.ToLower -eq 'Y')) {
+        $accounts += $username
+      }
+
+      # Create new users
+      foreach ($user in $accounts) {
+        <# $user is the current item #>
+        New-LocalUser -Name $user -NoPassword
+      }
+
+      Write-Host "$($accounts.Count) new accounts were created successfully"
+    }
   } else {
     Write-Host "Sorry, ${Filename} does not exist."
   }
 }
+
+
+# Write-Host "PSBoundParameters.Count: $($PSBoundParameters.Count)" # int
+# Write-Host "Args.Count: $($Args.Count)" # int
