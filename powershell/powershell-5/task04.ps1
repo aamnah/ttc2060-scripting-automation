d83897168b1fc248fcad55c17d9c3f20e928ff31
Aamnah <hello@aamnah.com>
2023-10-18 20:43:44 +0300

    param

--- powershell/powershell-5/task04.ps1
+++ powershell/powershell-5/task04.ps1
@@ -1,5 +1,12 @@
 <# 5.4
 [ ] Create a script that writes information of all the local users in a computer to a CSV-file. 
-[ ] The script has one parameter filename. 
+[x] The script has one parameter filename. 
 [ ] The information includes local users's 1) name 2) full name 3) SID and 4) lastlogon. 
 #>
+
+param(
+  [Parameter(Mandatory,
+  HelpMessage = "Provide the file to save local user details in.")]
+  [string]$Filename = 'localusers.csv'
+)
+
