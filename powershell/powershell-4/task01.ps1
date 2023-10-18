437b0c3a37f4f5fdd629690dd81c3de1713428c0
Aamnah <hello@aamnah.com>
2023-10-18 09:33:48 +0300

    initial

--- powershell/powershell-4/task01.ps1
+++ powershell/powershell-4/task01.ps1
@@ -1,1 +1,7 @@
 <# Create a function named Get-FiluInfo that shows Name and Length properties of all files in user's home folder. Run the function to prove that it works. #>
+
+function Get-FileInfo {
+  Get-ChildItem -Path ${HOME} | Format-Table -Property Name, Length
+}
+
+Get-FileInfo
