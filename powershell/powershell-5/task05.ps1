17c5f4bb649f02b519f66cb876c354022c6cbb1f
Amna <AD9795@student.jamk.fi>
2023-10-21 13:38:50 +0200

    tested

--- powershell/powershell-5/task05.ps1
+++ powershell/powershell-5/task05.ps1
@@ -2,20 +2,34 @@
 [x] In this task you have to create a new function called Add-MyEvent. 
 [x] The function takes two parameter: Type and Message. 
 [x] The type can be: Error, Warning, or Information. 
-[ ] Message is a clear message. 
-[ ] Add the following functionality to the task 04 using your new function. 
-  [ ] - if the script can create a new CSV-file succesfully, then it will add a Information message to the eventlog. 
-  [ ] - if the script cannot create a file succesfully, then it adds a Error message to the eventlog.
-[ ] Before using your new function create a new source named "MyPowerShell" to the Evenlog for writing your events. 
+[x] Message is a clear message. 
+[x] Add the following functionality to the task 04 using your new function. 
+  [x] - if the script can create a new CSV-file succesfully, then it will add a Information message to the eventlog. 
+  [x] - if the script cannot create a file succesfully, then it adds a Error message to the eventlog.
+[x] Before using your new function create a new source named "MyPowerShell" to the Evenlog for writing your events. 
 #>
 
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-7.3#validateset-attribute
 
 function Add-MyEvent() {
   param (
+    [Parameter(Mandatory)]
     [ValidateSet("Error", "Warning", "Information")]
-    [string]$Type = '',
-    [string]$Message = ''
+    [string]$Type,
+    [Parameter(Mandatory)]
+    [string]$Message, # Mandatory params are left uninitialized
+    [string]$LogName = 'Application',
+    [string]$Source = 'MyPowerShell',
+    [int32]$EventId = 1
   )
 
+  # Add a new Event
+  Write-EventLog -LogName $LogName -Source $Source -EntryType $Type -Message $Message -EventId $EventId
+  
 }
+
+# Test Run:
+# Add-MyEvent -Type Information -Message 'This is coming from the powershell script'
+
+# Register a new source
+# New-EventLog -LogName $LogName -Source $Source
