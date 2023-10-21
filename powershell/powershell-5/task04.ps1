75a447109a295fa0b09d7afa5ff4925ea50b8d33
Amna <AD9795@student.jamk.fi>
2023-10-21 13:39:06 +0200

    + logging

--- powershell/powershell-5/task04.ps1
+++ powershell/powershell-5/task04.ps1
@@ -6,13 +6,23 @@
  [x] 2) full name 
  [x] 3) SID and 
  [x] 4) lastlogon. 
+
+[x] Add EventLog functionality from Task 05 to log success/fail of CSV file creation
 #>
 
 param(
   [Parameter(Mandatory,
   HelpMessage = "Provide the file to save local user details in. For example: /home/user/localusers.csv")]
   [string]$Filename = ''
-)
+  )
+
+# Source `Add-MyEvent` from Task 5
+# Dot source works just like Bash
+# . .\task05.ps1
+# Join path makes the path cross-platform (\ vs /)
+# although it doesn't matter so much in this particular script
+# where we are using Windows only functionality (i.e. Get-LocalUser)
+. (Join-Path $PSScriptRoot 'task05.ps1')
 
 function Export-LocalUser() {
   param(
@@ -23,14 +33,20 @@ function Export-LocalUser() {
 }
 
 if (Test-Path $Filename) {
-  # The default behavior of `Export-Csv` is to overwrite any existing files. Caution about this.
+  # The default behavior of `Export-Csv` is to overwrite any existing files. 
+  # Caution the user about this and ask if overwriting is okay
   $consent = Read-Host "$Filename already exists. Do you want to overwrite? [Yes/No]"
 
   if (($consent.ToLower() -eq 'yes') -Or ($consent.ToLower() -eq 'y')) {
     Export-LocalUser -OutFile $Filename
+    Add-MyEvent -Type Information -Message "CSV file $Filename created successfully."
+  } else {
+    Add-MyEvent -Type Information -Message "Could not create CSV file: $Filename"
   }
 } else {
+  # CSV file doesn't exist, create a new one
   Export-LocalUser -OutFile $Filename
+  Add-MyEvent -Type Information -Message "CSV file $Filename created successfully."
 }
 
 
