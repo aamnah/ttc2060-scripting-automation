b4f1298fa537c3f4b7c2c6b399a889f61d15487b
Amna <ad9795@student.jamk.fi>
2023-11-15 10:41:30 +0200

    tested

--- shell/shell-2/task4.sh
+++ shell/shell-2/task4.sh
@@ -6,3 +6,31 @@ Use the following syntax in you conditional statement: if id -u USERNAME. If the
 New user must have home directory under the /home/ directory and default shell as bash.
 Hint: User creation needs sudo permissions, how to execute script with it?
 task_24_brief
+
+# need to run script with SUDO, otherwise user creation will not work
+
+USERNAME=''
+
+create_user() {
+  read -p "Please provide a username: " USERNAME
+
+  # by default user ID will be returned to the console. e.g. 1000
+  # redirect to /dev/null to get rid of that, so that only the output we want is shown
+  if id -u "$USERNAME" >/dev/null 2>&1
+  then
+    echo "${USERNAME} already exists. Exiting."
+  else 
+    useradd ${USERNAME} --create-home --shell=/bin/bash
+    echo -e "User ${USERNAME} successfully created. Listing /home/.. \n"
+    ls -HF /home/
+  fi
+}
+
+delete_user() {
+  sudo userdel $1
+  sudo rm -r /home/$1
+}
+
+create_user
+
+#delete_user $USERNAME
