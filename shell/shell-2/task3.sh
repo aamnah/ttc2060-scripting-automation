52c8001e9fb404d84f65ad1fd8aabd4a44e60d41
Amna <ad9795@student.jamk.fi>
2023-11-15 09:21:14 +0200

    init

--- shell/shell-2/task3.sh
+++ shell/shell-2/task3.sh
@@ -1,4 +1,17 @@
 #!/bin/bash 
 <<task_23_brief
-  
+Create a script that returns a type of objects given (file or directory or something else).
+User can give unlimited number of paths using the commandline arguments.
+Every object type is printed on the own line.
 task_23_brief
+
+find_object_type() {
+  # loop over all arguments provided by the user
+  for item in "$@"
+  do
+  # Print the file type
+    file ${item}
+  done
+}
+
+find_object_type
