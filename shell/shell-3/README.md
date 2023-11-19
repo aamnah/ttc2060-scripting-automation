f1ff071678723046f05725637fde19fc1588c5af
Amna <akram.aamnah@gmail.com>
2023-11-19 14:38:57 +0200

    tested

--- shell/shell-3/README.md
+++ shell/shell-3/README.md
@@ -7,6 +7,7 @@ Related topics:
 ## Task 1
 Create a script that asks values from the user until user types exit. All the values are saved in array. Finally script prints out the whole array (line by line) using the following syntax: `index: value`.
 
+![task 1](task1.png)
 
 ## Task 2
 Create a script that prints a number of objects in the directory where you executed the script from.
@@ -15,12 +16,14 @@ Script must consist a function called `count_objects`. Function must have one lo
 
 Hints: Function has a print functionality. `wc` command might be useful.
 
+![task 2](task2.png)
 
 ## Task 3
 Modify Task 2.
 
 Create a script that prints a number of objects in a **given directory** to the user. The path is asked from the user.
 
+![task 3](task3.png)
 
 ## Task 4
 Modify Task 3.
@@ -31,8 +34,11 @@ Function takes an array as an argument.
 
 Hints: **read** commands options might be useful or some kind of loop.
 
+![task 4](task4.png)
 
 ## Task 5
 Modify **Exercise set 2, Task 5** (Create a script that mimics a simple calculator).
 
 Change the logic that all operations are calculated by using functions.
+
+![task 5](task5.png)
--- shell/shell-3/task1.sh
+++ shell/shell-3/task1.sh
@@ -1,4 +1,43 @@
 #!/bin/bash 
 <<task_31_brief
-  
+[x] Create a script that asks values from the user until user types exit. 
+[x] All the values are saved in array. 
+[x] Finally script prints out the whole array (line by line) using the following syntax: "index: value".
 task_31_brief
+
+#new indexed array defination with a declare builtin command and manual value assignment
+# () can be used for compound assignments
+# -a is or indexed arrays
+# -A is for associative arrays (dictionaries)
+declare -a DATARAY
+
+INPUT=''
+
+ask_for_value() {
+  read -p "Please enter a value: " INPUT
+  DATARAY+=(${INPUT})
+}
+
+ask_for_value
+
+# Check uf user typed exit
+if [ $(echo "${INPUT}" | tr "[:upper:]" "[:lower:]") == "exit" ]
+# $(echo "${INPUT}" | tr "[:upper:]" "[:lower:]")
+# is to check for the word in a case-insensitive way
+# will check for Exit, exit or eXiT
+then
+  exit
+else
+  # Keep asking for input until the user enters exit
+  until [ $(echo "${INPUT}" | tr "[:upper:]" "[:lower:]") == "exit" ]
+  do
+    ask_for_value
+  done
+fi
+
+# Output
+for x in "${!DATARAY[@]}"
+do
+  # inedx: value
+  echo "${x}: ${DATARAY[$x]}"
+done
