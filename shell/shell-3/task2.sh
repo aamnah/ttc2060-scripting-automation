5fd292e63c5264520ea8d2788e50e9c3114049e0
Amna <akram.aamnah@gmail.com>
2023-11-19 14:50:00 +0200

    tested

--- shell/shell-3/task2.sh
+++ shell/shell-3/task2.sh
@@ -1,4 +1,15 @@
 #!/bin/bash 
 <<task_32_brief
-  
+[x] Create a script that prints the number of objects in the directory where you executed the script from.
+
+[x] Script must consist a function called "count_objects". Function must have one local variable, that will hold the actual value, before it's printed to the user.
+
+Hints: Function has a print functionality. "wc" command might be useful.
 task_32_brief
+
+count_objects() {
+  COUNT=$(ls | wc -l)
+  echo $COUNT
+}
+
+count_objects
