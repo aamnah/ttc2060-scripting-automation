7fbb7301370fbe7bb6bf7160958c6bba78b6702d
Amna <akram.aamnah@gmail.com>
2023-11-19 14:50:08 +0200

    tested

--- shell/shell-3/task3.sh
+++ shell/shell-3/task3.sh
@@ -1,4 +1,17 @@
 #!/bin/bash 
 <<task_33_brief
-  
+Modify Task 2.
+
+[x] Create a script that prints a number of objects in a **given directory** to the user. 
+[x] The path is asked from the user.
 task_33_brief
+
+read -p 'Please provide a directory path: ' DIR
+
+count_objects() {
+  COUNT=$(ls $1 | wc -l)
+  
+  echo "There are $COUNT objects in the provided directory"
+}
+
+count_objects $DIR
