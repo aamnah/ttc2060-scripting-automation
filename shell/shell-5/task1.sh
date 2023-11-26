63d6ad8eb2cfc4f32a50664af7d47fe8498beb49
Amna <akram.aamnah@gmail.com>
2023-11-26 17:19:33 +0200

    tested

--- shell/shell-5/task1.sh
+++ shell/shell-5/task1.sh
@@ -1,4 +1,36 @@
 #!/bin/bash 
 <<task_51_brief
-  
+[ ] Create a script that asks values from the user until user decides to end the script.  
+[ ] Given values are added to the end of file called 'animal.txt'
+[ ] Finally script prints every value from the file.  
 task_51_brief
+
+FILE="animal.txt"
+INPUT=""
+
+function aks_for_value {
+  read -p "Give me an animal name, or enter 'q' to end the script: " INPUT
+}
+
+
+to_lower() {
+  # $(echo "${INPUT}" | tr "[:upper:]" "[:lower:]")
+# is to check for the word in a case-insensitive way
+# will check for Exit, exit or eXiT
+  echo "${1}" | tr "[:upper:]" "[:lower:]"
+}
+
+
+while true
+do
+  aks_for_value
+  if [ $(to_lower "$INPUT") ==  "q" ]
+  then
+    break
+  else
+    echo -e "$INPUT" >> $FILE
+  fi
+done
+
+echo -e "\nThe values in $FILE are: \n"
+cat $FILE
