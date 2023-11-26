ccecfa672a421bbb735a47b39fd078b15ada5cb4
Amna <akram.aamnah@gmail.com>
2023-11-26 15:01:42 +0200

    tested +screenshot

--- shell/shell-4/task3.sh
+++ shell/shell-4/task3.sh
@@ -1,4 +1,25 @@
 #!/bin/bash 
 <<task_43_brief
-  
+[x] Create a script that installs the program using apt, based on the user input. (cowsay etc.)
+[x] If the installation is successfull, tell that to the user and exit normally.
+[x] If not, inform user and exit with a code 111.
+Hints: sudo permissions and apt options
 task_43_brief
+
+
+function install_package {
+  read -p "What program would you like to install? " PACKAGE
+
+  sudo apt install -qq $PACKAGE
+
+  if [ ${?} -ne 0 ]
+  then
+    echo -e "\nFailed to install $PACKAGE\n"
+    exit 111
+  else
+    echo -e "\nSuccessfully installed $PACKAGE\n"
+    exit
+  fi
+}
+
+install_package
