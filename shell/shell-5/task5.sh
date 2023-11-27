52caad9b3ef862206806fa7335c84e07a8334f9f
Amna <akram.aamnah@gmail.com>
2023-11-27 20:20:37 +0200

    wip

--- shell/shell-5/task5.sh
+++ shell/shell-5/task5.sh
@@ -1,11 +1,70 @@
 #!/bin/bash 
 <<task_55_brief
-[ ] Create a script that reads [superhero.json] file.
-[ ] Script prints hero names to the user who selects which heroes powers or secret identity he/she wants to see.
+[x] Create a script that reads [superhero.json] file.
+[x] Script prints hero names to the user who selects which heroes powers or secret identity he/she wants to see.
 [ ] Script prints the desired values to the user.
 [ ] Script will continue asking what to do till user gives some string to exit to script (exit perhaps?).
 
 Hints:
 - any iterations structure
-- `jq` command
-task_55_brief 
+- 'jq' command
+task_55_brief
+
+
+FILE="superhero.json"
+HEROES_LENGTH=$(( $(jq ".members | length" $FILE) + 0 ))
+
+HEROES=()
+
+# Populate the Heroes array
+COUNT=0
+while [ $COUNT -lt $HEROES_LENGTH ]
+do
+  NAME=$(cat $FILE | jq ".members[$COUNT].name")
+  # echo $NAME
+  HEROES+=("$NAME")
+
+  ((COUNT++))
+done
+
+# Show a select menu for Heroes
+PS3="Which hero do you want to know? "
+select hero in "${HEROES[@]}" Exit
+do
+  if [ "$hero" == "Exit" ]
+  then
+    break
+  fi
+
+  # The read line is saved in the REPLY variable.
+  INDEX=$(($REPLY - 1)) # index is 0-based while select menu starts at 1
+  MEMBER=$(jq ".members[$INDEX]" $FILE)
+  SECRET_IDENTITY=$(jq ".members[$INDEX].secretIdentity" $FILE)
+  POWERS=$(jq ".members[$INDEX].powers" $FILE)
+
+  echo -e "\nThe secret identity of ${hero} is ${SECRET_IDENTITY}. Powers are:"
+  echo "$POWERS"
+done
+
+
+# this block does NOT work because of {0..$VAR}
+# for x in {0.."$HEROES_LENGTH"}
+# do
+#   NAME=$(cat $FILE | jq ".members[$x].name")
+#   echo $NAME
+#   HEROES+=("$NAME")
+# done
+
+# echo "Which hero do you want to know? "
+# for KEY in "${!HEROES[@]}"
+# do
+#   echo "${KEY}: ${HEROES[$KEY]}"
+# done
+
+
+
+
+# for KEY in "${!array[@]}"
+# do
+#   echo "${KEY}: ${array[$KEY]}",
+# done
